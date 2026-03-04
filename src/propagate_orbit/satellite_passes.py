8c5c63ef1099161604efc1de2b82e451afe5cce1
Kayla Wong <kwong261@calpoly.edu>
2026-03-03 18:59:34 -0800

    max elevation and az detector

--- src/propagate_orbit/satellite_passes.py
+++ src/propagate_orbit/satellite_passes.py
@@ -26,6 +26,7 @@ from yaml import events
 from src.setup import setup_orekit
 setup_orekit()
 from math import radians
+from math import degrees
 
 #import orekit libraries
 from org.orekit.bodies import OneAxisEllipsoid, GeodeticPoint
@@ -76,31 +77,74 @@ def detect_pass(name, latitude, longtiude, altitude, marconi_min_elevation,fixed
     passes = det.withHandler(ContinueOnEvent())
     
     #detected event/passes
-    return passes
+    return passes, topo
 
 
 
 #extract interval for aos/los from event detector
 def get_pass_intervals(pass_logger):
-    
-    #use EventLogger to extraxt time intervals for aos/los as list
-    events=list(pass_logger.getLoggedEvents())
-    utc = TimeScalesFactory.getUTC()
-   # ref = AbsoluteDate(1970, 1, 1, 0, 0, 0.0, utc)  # reference for durationFrom()
+    """
+    Returns intervals of all passes (aos to los) in chronological order
+    """
+    #get list of all aos/los passings
     events = list(pass_logger.getLoggedEvents())
 
-    #store all passes
+    #use time since absolute date to order events in chronological order (without this orekit wasn't
+    #ordering them the same)
+    utc = TimeScalesFactory.getUTC()
+    ref = AbsoluteDate(2026, 1, 1, 0, 0, 0.0, utc)
+    events.sort(key=lambda e: e.getState().getDate().durationFrom(ref))
+
     intervals = []
     aos = None
-    for ev in events: #go through events in order
-        date = ev.getState().getDate() #extract time
 
-        if ev.isIncreasing(): #AOS, eleveation is increasing above threshold
-            aos = date #store this time
-        else: #LOS, if decreasing past threshold
-            if aos is not None and date.compareTo(aos) > 0: #run after an AOS
+    for ev in events: #go through events in order
+        date = ev.getState().getDate() #extract time and state
+        
+        if ev.isIncreasing(): #AOS, elevation is increasing above threshold
+            aos = date #store time
+        else: #LOS, elevation is decreasing past threshold
+            if aos is not None and date.compareTo(aos) > 0: #los should be after aos
                 intervals.append((aos, date)) #store interval
             aos = None #reset next pass
 
-    #return list of intervals 
-    return intervals 
+    #retrun list of pass intervals        
+    return intervals
+
+
+
+def get_max_elevations(max_el_logger, intervals, topo, fixed_frame):
+    """
+    Returns list of max elevations for each pass in chronological order
+    """
+    max_events = list(max_el_logger.getLoggedEvents())
+
+    #sort by chronological order
+    utc = TimeScalesFactory.getUTC()
+    ref = AbsoluteDate(2026, 1, 1, 0, 0, 0.0, utc)
+    max_events.sort(key=lambda e: e.getState().getDate().durationFrom(ref))
+
+    max_list = []
+    for (aos, los) in intervals:
+        best_el = float("nan") #best defaults nan until max found for each pass
+        best_az = float("nan")
+
+        for ev in max_events:
+            st = ev.getState() #spacecraft state
+            date = st.getDate() #date of event
+
+            #for each aos los window
+            if date.compareTo(aos) >= 0 and date.compareTo(los) <= 0:
+                
+                pos_fixed = st.getPVCoordinates(fixed_frame).getPosition() #get spacrcraft position in fixed frame
+
+                elev = degrees(topo.getElevation(pos_fixed, fixed_frame, date)) #get elevation of spacraft
+                az = degrees(topo.getAzimuth(pos_fixed, fixed_frame, date)) #get azimuth of spacecraft relative to gs NSWE
+                
+                if (best_el != best_el) or (elev > best_el):  #if best is nan or elev is larger, keep best
+                    best_el = elev
+                    best_az = az
+
+        max_list.append({"max_el_deg": best_el, "az_at_max_el": best_az}) #store max elevation and azimuthfor each interval 
+
+    return max_list
